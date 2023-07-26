a033d42b5150f012235ec6a4e0f261c0bb3233ea
olivier.izad <olivier.i@us.ibm.com>
2023-07-25 23:06:49 -0700

    Finishing the assignment

--- p8-group-manager.py
+++ p8-group-manager.py
@@ -16,7 +16,9 @@ group_list = [
 
                 
 def welcome_msg():
+    print("\n\n")
     welcome_msg_2="This program creates groups with dynamic properties"
+    print("=" * len(welcome_msg_2))
     print("\t>> Welccome to Group Manager <<")
     print(welcome_msg_2)
     print("=" * len(welcome_msg_2))
@@ -24,65 +26,191 @@ def welcome_msg():
 def display_menu():
     print("?: list commands")
     print("C: Create a new group")
-    print("G: List group")
     print("A: Add data to a group")
+    print("G: List groups")
     print("L: List data for a group")
-    print("X: Exit\n")
+    print("X: Exit")
 
-def find_group(group_name):
-    # if len(group_name.strip()) > 0:
-    for grp in group_list:
-        if grp['group'] == group_name.strip():
-            print("Group already found, you will need a new group name")        
-    return False # Not found
-    
-def create_group():
-    group_name = input("Enter a group name (empty to cancel): ")
-    if len(group_name) == 0:
-        print("Need a name for your group.")
-        return
-    resp = find_group(group_name)
-    if resp == False:  # Not found
-        print("Creating new group...: ", group_name.strip())
-        new_group = {}
-        new_group["group"] = group_name
-        group_list.append(new_group)
-        print("New group added: ", group_name.strip())
+def find_group(d, group_name):
+    # If a group name is entered, validate the group doesn't already exist.
+    does_group_exist = False
+    for key in d:
+        # If the group does exist, print a warning and re-prompt
+        if key.lower() == group_name.lower():
+            does_group_exist = True
+            break
+    return does_group_exist
+
+
+def create_group(d):
+    print("** Create new group **")
+
+    while True:
+
+        does_group_exist = True
+        while does_group_exist:
+
+            group_name = input("\nEnter group name (empty to cancel): ").strip()
+
+            # If nothing is entered, exit the Create New Group.
+            if len(group_name) == 0:
+                return
+
+            # If a group name is entered, validate the group doesn't already exist.
+            does_group_exist = find_group(d, group_name)
+
+            if does_group_exist:
+                print("[WARNING] Group already exists")
+
+            # If does_group_exist is False, the inner while loop will end.
+
+
+        # If the group doesn't exist, prompt for property names
+        # This portion of the code still needs to be inside of the outer while loop because
+        # the directions indicate a "group name" re-prompt after entering the field names
+        field_names = []
+        while True:
+            field_name = input("Enter field name (empty to stop): ").strip()
+            if len(field_name) > 0:
+                if field_name in field_names:
+                    print("[WARNING] Field already exists")
+                else:
+                    field_names.append(field_name)
+            else:
+                break
+
+        # When nothing is entered, save the group and the properties for that group    
+        d[group_name] = {"_fields_": field_names, "_data_": []}
             
-def list_groups():
-    if len(group_list) == 0:
+def list_groups(d):
+    print("** List of groups **")
+
+    if len(d) == 0:
         print("No groups found\n")
     else:
-        i = 1
-        for grp in group_list:
-            print(str(i) + ". " + grp["group"]) # + " (" + str(grp[1]) + ")")
-            i += 1
-        print()
+        # Display a sorted list of groups
+        sorted_group_names = list(d.keys())
+        sorted_group_names.sort()
         
-def add_group_data():
-    pass
+        for group_name in sorted_group_names:
 
-def list_group_data():
-    pass
+            # Display the group name
+            display_text = group_name
+
+            # Display the number of properties
+            properties = d[group_name]["_fields_"]
+            number_of_properties = len(properties)
+
+            # Display a comma-delimited list of property names
+            all_the_properties = (", ".join(properties)).strip()
+
+            # All together
+            display_text += " : " + str(number_of_properties) + " properties"
+            display_text += " (" + all_the_properties + ")"
+
+            print(display_text)
+        
+def add_group_data(d):
+    print("** Add group data **")
+
+    # Display sorted list of groups (tip: call the list_groups() function)
+    list_groups(d)
+
+    while True:
+        
+        does_group_exist =  False
+        group_name = ""
+
+        # Prompt for a group name
+        while not does_group_exist:
+            group_name = input("\nEnter group (empty to cancel): ").strip()
+
+            # When nothing is entered, leave the Add Group Data command
+            if len(group_name) == 0:
+                return
+
+            # If a group name is entered, validate the group exists
+            does_group_exist = find_group(d, group_name)
+
+            # If the group doesn't exist, display an error and re-prompt
+            if not does_group_exist:
+                print("[ERROR] Group does not exist")
+
+            # If does_group_exist is True, the inner while loop will end, otherwise, it will reprompt
+
+        # If the group does exist, prompt for input for each of the group properties
+        entry = {}
+        for field in d[group_name]["_fields_"]:
+            data_for_field = input("Enter " + field + ": ").strip()
+            entry[field] = data_for_field
+
+        # Save the properties for the group
+        d[group_name]["_data_"].append(entry)
+
+
+def list_group_data(d):
+    print("** List group data")
+
+    # Display sorted list of groups (tip: call the list_groups() function)
+    list_groups(d)
+
+    while True:
+
+        does_group_exist = False
+        group_name = ""
+
+        # Prompt for a group name
+        while not does_group_exist:
+
+            group_name = input("\nEnter group name (empty to cancel): ").strip()
+
+            # When nothing is entered, leave the List Data for Group command
+            if len(group_name) == 0:
+                return
+
+            # If a group name is entered, validate the group exists
+            does_group_exist = find_group(d, group_name)
+
+            # If the group doesn't exist, display an error and re-prompt
+            if not does_group_exist:
+                print("[ERROR] Group does not exist")
+
+            # If does_group_exist is True, the inner while loop will end, otherwise, it will reprompt
+
+
+        # If the group does exist, display a line for each stored group properties displaying the property and the property value
+        counter = 0
+        for entry in d[group_name]["_data_"]:
+            entry_array = []
+            for field in d[group_name]["_fields_"]:
+                value = entry[field]
+                entry_array.append(field + " = " + value)
+
+            print(str(counter) + " " + (", ".join(entry_array)).strip())
+            counter+=1
 
 def main():
     welcome_msg()
-    display_menu()
-    
+
+    d = {}
     while True:
-        cmd = input("Command (empty or C to quit, ? for help): ")
+        cmd = input("\nCommand (empty or X to quit, ? for help): ")
         if cmd == "?":
             display_menu()
         elif cmd.upper().strip() == "C":
-            create_group()            
-        elif cmd.upper().strip() == "L":
-            list_groups()
+            create_group(d)            
         elif cmd.upper().strip() == "A":
-            pass
+            add_group_data(d)
+        elif cmd.upper().strip() == "G":
+            list_groups(d)
         elif cmd.upper().strip() == "L":
-            pass
+            list_group_data(d)
         elif cmd.upper().strip() == "X" or len(cmd) == 0:
-            exit()            
+            exit()
+        else:
+            print("Command not recognized, please try again.")
+
+
 
 #run the main program
 if __name__ == "__main__":
